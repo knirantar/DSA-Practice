42defe13bc6c5cf7d7efba6f7af6077ac51d566a
knirantar <knirantar@gmail.com>
2024-05-19 13:33:28 +0530

    Difficult Matrix plus binary search

--- TwoDArray/MedianInRowWiseSortedMatrix.java
+++ TwoDArray/MedianInRowWiseSortedMatrix.java
@@ -10,7 +10,7 @@ public class MedianInRowWiseSortedMatrix {
                 { 4, 5, 6 },
                 { 7, 8, 9 }
         };
-        int median = findMedian(a);
+        int median = findMedianUsingBS(a);
         System.out.println(median);
     }
 
@@ -25,6 +25,56 @@ public class MedianInRowWiseSortedMatrix {
         }
         Arrays.sort(res);
         return res[n / 2];
+    }
+
+    private static int findMedianUsingBS(int[][] a) {
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
+        int n = a.length;
+        int m = a[0].length;
+
+        for (int i = 0; i < n; i++) {
+            low = Math.min(low, a[i][0]);
+            high = Math.max(high, a[i][m - 1]);
+        }
+
+        int req = (n * m) / 2;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int smallEqual = countSmallEqual(a, n, m, mid);
+            if (smallEqual <= req)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return low;
+    }
+
+    private static int countSmallEqual(int[][] a, int n, int m, int mid) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            count += ub(a[i], mid, m);
+        }
+        return count;
+    }
+
+    private static int ub(int[] a, int x, int m) {
+        int low = 0;
+        int high = m - 1;
+        int ans = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (a[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        if (ans == high && x > a[high])
+            return -1;
+        return ans;
     }
 
 }
