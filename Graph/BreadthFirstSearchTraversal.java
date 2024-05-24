e469eff7dfd8aba8ebd4ba12d5e828ccddeaae70
knirantar <knirantar@gmail.com>
2024-05-24 10:13:57 +0530

    Graph series started 24th May 2024

---
+++ Graph/BreadthFirstSearchTraversal.java
@@ -0,0 +1,55 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+import java.util.LinkedList;
+
+// To store graph we are going to use Matrix and List which are called 
+// Adjacency Matrix and Adjacency list
+// If we have 1 based idexing nodes then we will define the matrix with [n+1][n+1] dimensions
+// But we don't use Matrix as it wastes lot of space. We use List of list
+
+public class BreadthFirstSearchTraversal {
+    public static void main(String[] args) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            adj.add(new ArrayList<>());
+        }
+        adj.get(0).add(1);
+        adj.get(1).add(0);
+        adj.get(0).add(4);
+        adj.get(4).add(0);
+        adj.get(1).add(2);
+        adj.get(2).add(1);
+        adj.get(1).add(3);
+        adj.get(3).add(1);
+
+        bfs(5, adj);
+    }
+
+    private static void bfs(int V, List<List<Integer>> adj) {
+        List<Integer> bfs = new ArrayList<>();
+        boolean[] vis = new boolean[V];
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        vis[1] = true;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            bfs.add(node);
+
+            for (int i : adj.get(node)) {
+                if (!vis[i]) {
+                    q.add(i);
+                    vis[i] = true;
+                }
+            }
+        }
+        System.out.println(bfs);
+
+    }
+
+}
---
+++ Graph/Readme.md
@@ -0,0 +1,16 @@
+1. Types of Graph
+Undirected Graph
+Directed Graph
+
+2. Nodes - Edges
+
+3. Cycles in Graph - Start from a particular node and end at the same node
+
+4. Path - contains lot of nodes and edges and each of them are reachable. Node cannot appear twice in a path. Adjacent nodes must have edge between them.
+
+5. Degrees in Graph - Number of edges attached to the node in undirected graph is called Degree of Graph. Total degree of graph is twice the number of edges. In directed graph we have two types of degrees one is indegree and outdegree. Number of incoming edges on node are called indegree and number of outgoing edges are called outdegree
+
+6. Edge weight - Each edge in graph may have a particular weight. If the weights are not given we assume unit weight 1. 
+
+7. Connected Components - unconnected graph with multiple components. For any traversal algorithm we cannot get all the nodes without having a visited array and calling traversal to all unvisited nodes till all are visited.
+
