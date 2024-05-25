fa5c28c8618df7aaf4a3a4b8a408013ba1d8e566
knirantar <knirantar@gmail.com>
2024-05-25 22:35:32 +0530

    Find kth max with priority queue

--- Array/FindKthMax.java
+++ Array/FindKthMax.java
@@ -11,11 +11,14 @@ import java.util.PriorityQueue;
 public class FindKthMax {
     public static void main(String[] args) {
         int[] arr = new int[] { 12, 3, 5, 7, 19, 23 };
-        int kthsmallest = findKthMax(arr, 5);
-        System.out.println(kthsmallest);
+        int kthmax = findKthMax(arr, 2);
+        System.out.println(kthmax);
 
     }
 
+    // Priority queue stores in java as max heap
+    // So printing the queue will be confusing and will be with a structure of max
+    // heap
     private static int findKthMax(int[] arr, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for (int i = 0; i < arr.length; i++) {
