a9e9922b50b3e355effd556e7b5f44ff5c38cd70
knirantar <knirantar@gmail.com>
2024-05-25 21:52:58 +0530

    Added comments in cylic rotation of array

--- Array/CyclicallyRotateArray.java
+++ Array/CyclicallyRotateArray.java
@@ -9,13 +9,19 @@ public class CyclicallyRotateArray {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+        // 5,3,12,7,19,23
+        // 5,3,12,23,19,7
+        // 7,19,23,12,3,5
     }
 
     private static void rotateArray(int[] arr, int k) {
+        // First find out the actual number of rotations in the array using a modulo
+        // operator with length of array
         int r = k % arr.length;
         int i = 0;
         int j = r - 1;
 
+        // Reverse an array till rotations -1 element
         while (i < j) {
             swap(arr, i, j);
             i++;
