d04c8f976b97d4a786aa086fe1d7ff993fa3459b
knirantar <knirantar@gmail.com>
2024-05-26 00:52:24 +0530

    Largest Rectangle Correct implementation done

---
+++ Array/LargestRectangleAreaHistogram.java
@@ -0,0 +1,34 @@
+package Array;
+
+import java.util.Stack;
+
+public class LargestRectangleAreaHistogram {
+    public static void main(String[] args) {
+        int[] heights = { 2, 1, 5, 6, 2, 3 };
+        int maxArea = largestRectangleArea(heights);
+        System.out.println(maxArea); // Output should be 10
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int height = heights[stack.pop()];
+            int width = stack.isEmpty() ? n : n - stack.peek() - 1;
+            maxArea = Math.max(maxArea, height * width);
+        }
+
+        return maxArea;
+    }
+}
