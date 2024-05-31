8ad1119a4b97f8425e4c8e6631fdd87869f5c49b
knirantar <knirantar@gmail.com>
2024-05-31 18:16:00 +0530

    Longest common prefix

--- String/LongestCommonPrefix.java
+++ String/LongestCommonPrefix.java
@@ -1,5 +1,7 @@
 package String;
 
+import java.util.Arrays;
+
 /*
  * Write a function to find the longest common prefix string 
  * amongst an array of strings.
@@ -8,7 +10,26 @@ If there is no common prefix, return an empty string ""
  */
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-        String s = 
+        String[] s = new String[] { "ab", "a" };
+        String ans = findLongestCommonPrefix(s);
+        System.out.println(ans);
+
+    }
+
+    private static String findLongestCommonPrefix(String[] s) {
+        Arrays.sort(s);
+        String ans = "";
+        String first = s[0];
+        String last = s[s.length - 1];
+        for (int i = 0; i < s[0].length(); i++) {
+            if (first.charAt(i) == last.charAt(i)) {
+                ans = ans + first.charAt(i);
+            } else {
+                break;
+            }
+        }
+        return ans;
+
     }
 
 }
